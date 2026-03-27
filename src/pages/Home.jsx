8cd7a0c00a8e44b38931e1ff1ecbaade406589c0
jsilva <jsilva@agems.ms.gov.br>
2026-03-27 12:00:42 -0400

    feat: remove Pareceres Técnicos card for non-prestador users
    The card linking to "GestaoAutos?tab=analise" was removed from the home page dashboard as this functionality is being deprecated or moved elsewhere. This simplifies the user interface for non-prestador users.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -187,18 +187,6 @@ export default function Home() {
                     </Link>
 
                     {!isPrestador && (
-                        <Link to={`${createPageUrl('GestaoAutos')}?tab=analise`}>
-                            <Card className="bg-white/10 hover:bg-white/20 transition-all cursor-pointer border-white/20 h-full">
-                                <CardContent className="p-4 text-center">
-                                    <ClipboardCheck className="h-8 w-8 text-blue-300 mx-auto mb-2" />
-                                    <h3 className="text-white font-medium text-sm">Pareceres Técnicos</h3>
-                                    <p className="text-blue-300 text-xs">Analisar defesas</p>
-                                </CardContent>
-                            </Card>
-                        </Link>
-                    )}
-
-                    {!isPrestador && (
                         <Link to={createPageUrl('CamaraJulgamento')}>
                             <Card className="bg-white/10 hover:bg-white/20 transition-all cursor-pointer border-white/20 h-full">
                                 <CardContent className="p-4 text-center">
